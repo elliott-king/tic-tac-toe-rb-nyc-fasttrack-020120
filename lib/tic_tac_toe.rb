b4a381af81997391823856e7f419e8587c7f2475
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-02 18:36:40 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,68 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(arr, i, char='X')
+  arr[i] = char
+  arr
+end
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  # players = ["X", "O"]
+  WIN_COMBINATIONS.each do |combo|
+    first_position = board[combo[0]]
+    if !first_position.nil? && first_position != " "
+      if combo.all? {|space| board[space] == first_position}
+        return combo
+      end
+    end
+  end
+  false
+end
+
+def full?(board)
+  return board.all? {|space| space == "X" || space == "O"}
+end
+
+def draw?(board)
+  return full?(board) && !won?(board)
+end
+
+def over?(board)
+  return won?(board) || full?(board)
+end
+
+def winner(board)
+  combo = won?(board)
+  if combo
+    return board[combo[0]]
+  else
+    return nil
+  end
+end
+
+# Helper Method
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
